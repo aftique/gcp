dfb197c776774b316676692f9b9aa2f85ff73c00
aftique <107954414+aftique@users.noreply.github.com>
2022-06-21 14:24:23 -0500

    Update transformacion.py

--- transformacion.py
+++ transformacion.py
@@ -1,22 +1,3 @@
-
-gcloud dataproc clusters create dataproc-demo \
-                --bucket pruebas-trans \
-                --region us-central1 \
-                --zone us-central1-a \
-                --master-machine-type n1-standard-2 \
-                --master-boot-disk-size 100 \
-                --num-workers 2 \
-                --worker-machine-type n1-standard-2 \
-                --worker-boot-disk-size 50 \
-                --image-version 2.0-debian10 \
-                --scopes 'https://www.googleapis.com/auth/cloud-platform' \
-                --tags hadoopdemo --project ambiente-pruebas-353301 \
-                --initialization-actions gs://goog-dataproc-initialization-actions-us-central1/connectors/connectors.sh \
-                --metadata bigquery-connector-version=1.2.0 \
-                --metadata spark-bigquery-connector-version=0.21.0
-
-
-
 from pyspark.sql.functions import col
 from pyspark.sql.functions import sum
 from pyspark.sql import SparkSession
